b441a1d60e4deb16b61b179002d63a116d6c6836
Yuanqing Wang <wangyq@wangyq.net>
2019-04-30 18:52:10 -0400

    parsing smiles

--- gin/from_smiles.py
+++ gin/from_smiles.py
@@ -166,11 +166,13 @@ def smiles_to_organic_topological_molecule(smiles):
         length = tf.strings.length(smiles)
 
         # get the number of atoms
-        n_atoms = tf.strings.length(
-            tf.strings.regex_replace(
-                smiles,
-                N_ATOM_COUNTER_STR,
-                ''))
+        n_atoms = tf.cast(
+            tf.strings.length(
+                tf.strings.regex_replace(
+                    smiles,
+                    N_ATOM_COUNTER_STR,
+                    '')),
+            tf.int64)
 
         # initialize the adjacency map
         # the adjaceny map, by default,
@@ -190,6 +192,7 @@ def smiles_to_organic_topological_molecule(smiles):
         # deleting the first column and the last row
         # of an identity matrix with one more column & row
 
+        # (n_atoms, n_atoms)
         adjacency_map = tf.Variable(
             tf.eye(
                 n_atoms + 1,
@@ -225,7 +228,9 @@ def smiles_to_organic_topological_molecule(smiles):
     # =================================
     # translate atoms notations to idxs
     # =================================
+    # right now to depend on tf.strings to achieve this
     # TODO: further optimize this bit
+
     # carbon
     # aromatic or not
     smiles_atoms_only = tf.strings.regex_replace(
@@ -291,16 +296,17 @@ def smiles_to_organic_topological_molecule(smiles):
     # ==============================
     # handle the topology characters
     # ==============================
-
     smiles_topology_only = tf.strings.regex_replace(
         smiles,
         ALL_ORGANIC_ATOMS_STR,
         '0')
 
+    # split it into array
     smiles_topology_only = tf.string_split(
         [smiles_topology_only],
         '').values
 
+    # find all the indices and characters indicating topology
     topology_idxs = tf.reshape(
         tf.where(
             tf.not_equal(
@@ -312,7 +318,9 @@ def smiles_to_organic_topological_molecule(smiles):
         smiles_topology_only,
         topology_idxs)
 
+
     # map the topology idxs onto the atoms
+    # this is achieved by shifting the indicies of the topology characters
     topology_idxs = topology_idxs\
         - tf.range(
             tf.cast(
@@ -323,6 +331,7 @@ def smiles_to_organic_topological_molecule(smiles):
             1,
             dtype=tf.int64)
 
+
     # ===========
     # bond orders
     # ===========
@@ -331,21 +340,26 @@ def smiles_to_organic_topological_molecule(smiles):
     #       to multiple bonds
     # double bonds and triple bonds
     # modify the bond order to two or three
-    double_bond_idxs = tf.reshape(
-        tf.where(
-            tf.equal(
-                topology_chrs,
-                '=')),
-        [-1])
+    double_bond_idxs = tf.gather(
+        topology_idxs,
+        tf.reshape(
+            tf.where(
+                tf.equal(
+                    topology_chrs,
+                    '=')),
+            [-1]))
 
-    triple_bond_idxs = tf.reshape(
-        tf.where(
-            tf.equal(
-                topology_chrs,
-                '#')),
-        [-1])
+    triple_bond_idxs = tf.gather(
+        topology_idxs,
+        tf.reshape(
+            tf.where(
+                tf.equal(
+                    topology_chrs,
+                    '#')),
+            [-1]))
 
     # update double bonds
+    # (n_atoms, n_atoms)
     adjacency_map.scatter_nd_update(
         tf.transpose(
             tf.concat(
@@ -363,6 +377,7 @@ def smiles_to_organic_topological_molecule(smiles):
             dtype=tf.float32) * 2)
 
     # update triple bonds
+    # (n_atoms, n_atoms)
     adjacency_map.scatter_nd_update(
         tf.transpose(
             tf.concat(
@@ -486,11 +501,10 @@ def smiles_to_organic_topological_molecule(smiles):
 
         shape_invariants = [
             idx.get_shape(),
-            tf.TensorShape((None, )),
-            tf.TensorShape((None, 2))
-        ])
+            tf.TensorShape([None, ]),
+            tf.TensorShape([None, 2])])
 
-
+    # split into left and right brackets
     left_bracket_idxs = bracket_pairs[:, 0]
     right_bracket_idxs = bracket_pairs[:, 1]
 
@@ -508,10 +522,12 @@ def smiles_to_organic_topological_molecule(smiles):
             ],
             axis=0))
 
+    # get the current bond order
     current_bond_order = tf.gather_nd(
         adjacency_map,
         current_bond_idxs)
 
+    # calculate where should the new bonds be
     new_bond_idxs = tf.transpose(
         tf.concat(
             [
@@ -608,6 +624,7 @@ def smiles_to_organic_topological_molecule(smiles):
         [smiles_aromatic],
         '').values
 
+    # all the aromatic bonds where marked
     aromatic_idxs = tf.reshape(
         tf.where(
             tf.equal(
@@ -631,7 +648,7 @@ def smiles_to_organic_topological_molecule(smiles):
             ],
             axis=0))
 
-    current_bond_order = tf.gather(
+    current_bond_order = tf.gather_nd(
         adjacency_map,
         current_bond_idxs)
 
@@ -647,6 +664,404 @@ def smiles_to_organic_topological_molecule(smiles):
         # if there is no bond right now, then we don't do anything
         current_bond_order)
 
+    # update adjacency_map
+    adjacency_map.scatter_nd_update(
+        current_bond_idxs,
+        modified_bond_order)
+
+    # handle conjugate systems
+    adjacency_map_full = adjacency_map + tf.transpose(adjacency_map)
+
+    # =================
+    # conjugate systems
+    # =================
+    # search start with atoms connected to double bonds
+    sp2_idxs = tf.boolean_mask(
+        tf.range(n_atoms,
+            dtype=tf.int64),
+        tf.reduce_any(
+            tf.equal(
+                adjacency_map_full,
+                tf.constant(2.0, dtype=tf.float32)),
+            axis=1))
+
+    # also add aromatic idxs into our search
+    sp2_idxs = tf.concat(
+        [
+            sp2_idxs,
+            aromatic_idxs
+        ],
+        axis=0)
+
+    # NOTE:
+    # right now,
+    # we only allow carbon, nitrogen, or oxygen
+    # as part of our conjugate system
+    sp2_idxs = tf.boolean_mask(
+        sp2_idxs,
+        tf.logical_or(
+
+            tf.equal( # carbon
+                tf.gather(atoms, sp2_idxs),
+                tf.constant(
+                    0,
+                    dtype=tf.int64)),
+
+            tf.logical_or(
+                tf.equal( # nitrogen
+                    tf.gather(atoms, sp2_idxs),
+                    tf.constant(
+                        1,
+                        dtype=tf.int64)),
+
+                tf.equal( # oxygen
+                    tf.gather(atoms, sp2_idxs),
+                    tf.constant(
+                        2,
+                        dtype=tf.int64)))))
+
+    # gather only sp2 atoms
+    sp2_adjacency_map = tf.gather(
+        tf.gather(
+            adjacency_map_full,
+            sp2_idxs,
+            axis=0),
+        sp2_idxs,
+        axis=1)
+
+    # init conjugate_systems
+    conjugate_systems = tf.expand_dims(
+        tf.ones_like(sp2_idxs) \
+            * tf.constant(-1, dtype=tf.int64),
+        0)
+
+    # init visted flags
+    visited = tf.tile(
+        tf.expand_dims(
+            tf.constant(False),
+            0),
+        tf.expand_dims(
+            tf.shape(sp2_idxs)[0],
+            0))
+
+    def loop_body_inner(queue, visited, conjugate_system,
+            sp2_adjacency_map=sp2_adjacency_map):
+        # dequeue
+        idx = queue[-1]
+        queue = queue[:-1]
+
+        # flag the position of self
+        is_self = tf.equal(
+            tf.range(
+                tf.cast(
+                    tf.shape(sp2_adjacency_map)[0],
+                    tf.int64),
+                dtype=tf.int64),
+            idx)
+
+        # flag the neighbors
+        is_neighbors = tf.greater(
+            sp2_adjacency_map[idx, :],
+            tf.constant(0, dtype=tf.float32))
+
+        # flag the neighbors that are not visited
+        is_unvisited_neighbors = tf.logical_and(
+            is_neighbors,
+            tf.logical_not(
+                visited))
+
+        # check if self is visited
+        self_is_unvisited = tf.logical_not(visited[idx])
+
+        # put self in the conjugate system if self is unvisited
+        conjugate_system = tf.cond(
+            self_is_unvisited,
+
+            # if self.is_visited
+            lambda: tf.concat( # put self into the conjugate_system
+                [
+                    conjugate_system,
+                    tf.expand_dims(
+                        idx,
+                        0)
+                ],
+                axis=0),
+
+            # else:
+            lambda: conjugate_system)
+
+        # get the states of the neighbors
+        neighbors_unvisited = tf.boolean_mask(
+            tf.range(
+                tf.cast(
+                    tf.shape(sp2_adjacency_map)[0],
+                    tf.int64),
+                dtype=tf.int64),
+            is_unvisited_neighbors)
+
+        # append the undiscovered neighbors to the conjugate system
+        conjugate_system = tf.concat(
+            [
+                conjugate_system,
+                neighbors_unvisited
+            ],
+            axis=0)
+
+        # enqueue
+        queue = tf.concat(
+            [
+                queue,
+                neighbors_unvisited
+            ],
+            axis=0)
+
+        # change the visited flag
+        visited = tf.where(
+            tf.logical_or(
+                is_unvisited_neighbors,
+                is_self),
+
+            # if sp2_atom is neighbor and is unvisited:
+            tf.tile(
+                tf.expand_dims(
+                    tf.constant(True),
+                    0),
+                tf.expand_dims(
+                    tf.shape(is_unvisited_neighbors)[0],
+                    0)),
+
+            # else:
+            visited)
+
+        return queue, visited, conjugate_system
+
+    def loop_body_outer(
+            conjugate_systems,
+            visited,
+            sp2_adjacency_map=sp2_adjacency_map):
+
+        # start with the first element that's not visited
+        queue = tf.expand_dims(
+            tf.boolean_mask(
+                tf.range(
+                    tf.cast(
+                        tf.shape(sp2_adjacency_map)[0],
+                        tf.int64),
+                    dtype=tf.int64),
+                tf.logical_not(visited))[0],
+            0)
+
+        # init conjugate system
+        conjugate_system = tf.constant([], dtype=tf.int64)
+
+        queue, visited, conjugate_system = tf.while_loop(
+            # while len(queue) > 0:
+            lambda queue, visited, conjugate_system: tf.greater(
+                tf.shape(queue)[0],
+                tf.constant(0, tf.int32)),
+
+            # execute inner loop body
+            loop_body_inner,
+
+            # loop var
+            [queue, visited, conjugate_system],
+
+            shape_invariants=[
+                tf.TensorShape([None, ]),
+                tf.TensorShape(visited.get_shape()),
+                tf.TensorShape([None, ])])
+
+        conjugate_system = tf.cond(
+            # check if there are at least three atoms in the conjugate system
+            tf.greater_equal(
+                tf.shape(conjugate_system)[0],
+                tf.constant(3, dtype=tf.int32)),
+
+            # if there is more than three atoms in the conjugate system:
+            lambda: conjugate_system,
+
+            # else:
+            lambda: tf.constant([], dtype=tf.int64))
+
+        # pad it with -1
+        conjugate_system = tf.concat(
+            [
+                conjugate_system,
+                tf.ones(tf.shape(sp2_idxs)[0] \
+                    - tf.shape(conjugate_system)[0],
+                    dtype=tf.int64) \
+                    * tf.constant(-1, dtype=tf.int64)
+            ],
+            axis=0)
+
+        # put conjugate system into the list of systems
+        conjugate_systems = tf.concat(
+            [
+                conjugate_systems,
+                tf.expand_dims(
+                    conjugate_system,
+                    0)
+            ],
+            axis=0)
+
+        return conjugate_systems, visited
+
+    conjugate_systems, visited = tf.while_loop(
+        # while not all are visited
+        lambda conjugate_systems, visited: tf.reduce_any(
+            tf.logical_not(
+                visited)),
+
+        # loop_body
+        loop_body_outer,
+
+        # loop var
+        [conjugate_systems, visited],
+
+        shape_invariants=[
+            tf.TensorShape([None, sp2_idxs.shape[0]]),
+            tf.TensorShape((visited.get_shape()))])
+
+
+    # get rid of the empty entries
+    conjugate_systems = tf.boolean_mask(
+        conjugate_systems,
+        tf.reduce_any(
+            tf.greater(
+                conjugate_systems,
+                tf.constant(-1, dtype=tf.int64)),
+            axis=1))
+
+
+    # while loop
+    idx = tf.constant(0, dtype=tf.int32)
+    bond_idxs_to_update = tf.constant([[-1, -1]], dtype=tf.int64)
+    bond_orders_to_update = tf.constant([-1], dtype=tf.float32)
+
+    # update the bond order according to conjugate_systems
+    def loop_body(idx,
+            bond_idxs_to_update,
+            bond_orders_to_update,
+            conjugate_systems=conjugate_systems,
+            sp2_idxs=sp2_idxs,
+            sp2_adjacency_map=sp2_adjacency_map):
+
+        # get the conjugate system and get rid of the padding
+        conjugate_system = conjugate_systems[idx, :]
+        conjugate_system = tf.boolean_mask(
+            conjugate_system,
+            tf.greater(
+                conjugate_system,
+                tf.constant(-1, dtype=tf.int64)))
+
+        # get the bonds in the system
+        system_idxs = tf.gather(
+            sp2_idxs,
+            conjugate_system)
+
+        # get the full adjacency map
+        system_adjacency_map_full = tf.gather(
+            tf.gather(
+                sp2_adjacency_map,
+                conjugate_system,
+                axis=0),
+            conjugate_system,
+            axis=1),
+
+        # flatten it to calculate average bond
+        system_adjacency_map_flatten = tf.reshape(
+            tf.linalg.band_part(
+                system_adjacency_map_full,
+                0, -1),
+            [-1])
+
+        # get the average bond order and count
+        system_total_bond_order = tf.reduce_sum(system_adjacency_map_flatten)
+        system_total_bond_count = tf.reduce_sum(
+            tf.boolean_mask(
+                tf.ones_like(
+                    system_adjacency_map_flatten,
+                    dtype=tf.float32),
+                tf.greater(
+                    system_adjacency_map_flatten,
+                    tf.constant(0, dtype=tf.float32))))
+
+        system_mean_bond_order = tf.div(
+            system_total_bond_order,
+            system_total_bond_count)
+
+        # dirty stuff to get the bond indices to update
+        all_idxs_x, all_idxs_y = tf.meshgrid(
+            tf.range(n_atoms, dtype=tf.int64),
+            tf.range(n_atoms, dtype=tf.int64))
+
+        all_idxs_stack = tf.stack(
+            [
+                all_idxs_y,
+                all_idxs_x
+            ],
+            axis=2)
+
+        system_idxs_2d = tf.gather(
+            tf.gather(
+                all_idxs_stack,
+                system_idxs,
+                axis=0),
+            system_idxs,
+            axis=1)
+
+        system_idxs_2d = tf.boolean_mask(
+            system_idxs_2d,
+            tf.greater(
+                tf.gather_nd(
+                    adjacency_map,
+                    system_idxs_2d),
+                tf.constant(0, dtype=tf.float32)))
+
+        bond_idxs_to_update = tf.concat(
+            [
+                bond_idxs_to_update,
+                system_idxs_2d
+            ],
+            axis=0)
+
+        bond_orders_to_update = tf.concat(
+            [
+                bond_orders_to_update,
+                tf.ones(
+                    (tf.cast(system_total_bond_count, tf.int32), ),
+                    dtype=tf.float32) \
+                    * system_mean_bond_order
+            ],
+            axis=0)
+
+        return idx + 1, bond_idxs_to_update, bond_orders_to_update
+
+    _, bond_idxs_to_update, bond_orders_to_update = tf.while_loop(
+        # condition
+        lambda idx, _1, _2: tf.less(
+            idx,
+            tf.shape(conjugate_systems)[0]),
+
+        # loop body
+        loop_body,
+
+        # var
+        [idx, bond_idxs_to_update, bond_orders_to_update],
+
+        shape_invariants = [
+            idx.get_shape(),
+            tf.TensorShape([None, 2]),
+            tf.TensorShape([None, ])])
+
+    # get rid of the placeholder
+    bond_idxs_to_update = bond_idxs_to_update[1:]
+    bond_orders_to_update = bond_orders_to_update[1:]
+
+    # scatter update
+    adjacency_map.scatter_nd_update(
+        bond_idxs_to_update,
+        bond_orders_to_update)
 
 
     return atoms, adjacency_map
