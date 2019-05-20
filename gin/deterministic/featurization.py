445601a6e9fb96e3973c0147cc6f8709bc994bfd
Yuanqing Wang <wangyq@wangyq.net>
2019-05-20 17:38:00 -0400

    license

--- gin/deterministic/featurization.py
+++ gin/deterministic/featurization.py
@@ -1,8 +1,8 @@
 """
 MIT License
 
-Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center
-and Authors
+Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center,
+Weill Cornell Medical College, Nicea Research, and Authors
 
 Authors:
 Yuanqing Wang
@@ -24,6 +24,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 """
 
 # =============================================================================
--- tonic/nets/for_gn.py
+++ tonic/nets/for_gn.py
@@ -6,8 +6,8 @@ and update and aggregation functions for graph nets.
 
 MIT License
 
-Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center
-and Authors
+Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center,
+Weill Cornell Medical College, Nicea Research, and Authors
 
 Authors:
 Yuanqing Wang
@@ -29,6 +29,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 """
 
 # ===========================================================================
