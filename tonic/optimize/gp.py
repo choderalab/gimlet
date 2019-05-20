4fe03fcf5c41f7923eb91bfbcff815fdd3dfd453
Yuanqing Wang <wangyq@wangyq.net>
2019-05-20 17:38:00 -0400

    license

--- tonic/optimize/gp.py
+++ tonic/optimize/gp.py
@@ -5,8 +5,8 @@ Bayesian Optimization Using Gaussian Process.
 
 MIT License
 
-Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center
-and Authors
+Copyright (c) 2019 Chodera lab // Memorial Sloan Kettering Cancer Center,
+Weill Cornell Medical College, Nicea Research, and Authors
 
 Authors:
 Yuanqing Wang
@@ -28,6 +28,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 """
 
 # =============================================================================
