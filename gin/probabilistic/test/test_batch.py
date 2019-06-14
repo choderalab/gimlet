634446139d9afe03ecf9957e406496348e434c06
Yuanqing Wang <wangyq@wangyq.net>
2019-06-13 17:01:15 -0700

    batched graph nets

---
+++ gin/probabilistic/test/test_batch.py
@@ -0,0 +1,20 @@
+import pytest
+import gin
+import numpy as np
+import numpy.testing as npt
+import tensorflow as tf
+import pandas as pd
+
+
+# read data
+df = pd.read_csv('data/delaney-processed.csv')
+x_array = df[['smiles']].values.flatten()
+y_array = df[['measured log solubility in mols per litre']].values.flatten()
+y_array = (y_array - np.mean(y_array) / np.std(y_array))
+
+ds = gin.i_o.from_smiles.smiles_to_mols_with_attributes(x_array, y_array)
+
+ds = gin.probabilistic.gn.GraphNet.batch(ds, 256)
+
+for x in ds:
+    print(x)
