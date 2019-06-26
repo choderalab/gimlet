0a228ad75462c6f37ec065c4e6638ab12daa0e41
Yuanqing Wang <wangyq@wangyq.net>
2019-06-26 11:11:47 -0400

    fixed bugs

--- gin/deterministic/test/test_md.py
+++ gin/deterministic/test/test_md.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import gin
 
 caffeine = gin.i_o.from_smiles.smiles_to_organic_topological_molecule('CN1C=NC2=C1C(=O)N(C(=O)N2C)C')
+caffeine = gin.deterministic.hydrogen.add_hydrogen(caffeine)
 caffeine_md_system = gin.deterministic.md.SingleMoleculeMechanicsSystem(caffeine)
 
 caffeine_md_system.minimize()
