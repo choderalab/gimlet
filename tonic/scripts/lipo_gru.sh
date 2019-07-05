dedd5debe11cccf2a5582a4f9d160dfb5138d91a
yuanqing-wang <wangyq@wangyq.net>
2019-07-05 14:56:47 -0400

    fixed bugs

--- tonic/scripts/lipo_gru.sh
+++ tonic/scripts/lipo_gru.sh
@@ -1,13 +1,13 @@
 #BSUB -q gpuqueue
-#BSUB -J lipo_vanilla
+#BSUB -J lipo_gru
 #BSUB -m "ld-gpu ls-gpu lt-gpu lp-gpu lg-gpu lv-gpu lu-gpu"
 #####BSUB -m "lu-gpu"
 #BSUB -q gpuqueue -n 12 -gpu "num=4:j_exclusive=yes"
-#BSUB -R "rusage[mem=4] span[ptile=12]"
+#BSUB -R "rusage[mem=4] span[hosts=1]"
 ######BSUB -R V100
 #BSUB -W 24:00
 #BSUB -o %J.stdout
 #BSUB -eo %J.stderr
 
 module add cuda/10.0
-python ht_lipo_vanilla.py
+python ht_lipo_gru.py
--- tonic/scripts/lipo_vanilla.sh
+++ tonic/scripts/lipo_vanilla.sh
@@ -2,10 +2,10 @@
 #BSUB -J lipo_vanilla
 #BSUB -m "ld-gpu ls-gpu lt-gpu lp-gpu lg-gpu lv-gpu lu-gpu"
 #####BSUB -m "lu-gpu"
-#BSUB -q gpuqueue -n 12 -gpu "num=1:j_exclusive=yes"
+#BSUB -q gpuqueue -n 12 -gpu "num=4:j_exclusive=yes"
 #BSUB -R "rusage[mem=4] span[ptile=12]"
 ######BSUB -R V100
-#BSUB -W 160:00
+#BSUB -W 24:00
 #BSUB -o %J.stdout
 #BSUB -eo %J.stderr
 
