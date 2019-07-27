bbd54bf04a2ac68bee9709bd2f52faf4f53623b8
Yuanqing Wang <wangyq@wangyq.net>
2019-07-27 09:18:18 +0800

    train as a function

--- gin/probabilistic/test/test_gn_backprop.py
+++ gin/probabilistic/test/test_gn_backprop.py
@@ -1,28 +1,123 @@
 import gin
 import lime
+import logging
 import tensorflow as tf
 # tf.enable_eager_execution()
 import pandas as pd
 import numpy as np
+import sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+logger = tf.get_logger()
 
 # read data
 df = pd.read_csv('data/delaney-processed.csv')
+df = df[df['smiles'].str.len() > 1]
 x_array = df[['smiles']].values.flatten()
 y_array = df[['measured log solubility in mols per litre']].values.flatten()
 y_array = (y_array - np.mean(y_array) / np.std(y_array))
 
 ds = gin.i_o.from_smiles.to_mols_with_attributes(x_array, y_array)
-
+ds = gin.probabilistic.gn.GraphNet.batch(ds, 256)
 
 class f_r(tf.keras.Model):
-    def __init__(self, config):
+    def __init__(self, gru_unit, d_config):
         super(f_r, self).__init__()
-        self.d = lime.nets.for_gn.ConcatenateThenFullyConnect(config)
+
+        # edges
+        self.gru_e = tf.keras.layers.GRU(
+            gru_unit,
+            return_state=True,
+            kernel_initializer='RandomUniform')
+
+        # vertices
+        self.gru_v = tf.keras.layers.GRU(
+            gru_unit,
+            return_state=True,
+            kernel_initializer='RandomUniform')
+
+        # universal
+        self.gru_u = tf.keras.layers.GRU(
+            gru_unit,
+            return_state=True,
+            kernel_initializer='RandomUniform')
+
+        self.d = lime.nets.for_gn.ConcatenateThenFullyConnect(d_config)
 
     @tf.function
     def call(self, h_e, h_v, h_u,
-            h_e_history, h_v_history, h_u_history, _1, _2):
-        y = self.d(h_u)[0][0]
+            h_e_history, h_v_history, h_u_history,
+            atom_in_mol, bond_in_mol):
+
+        h_e_history.set_shape([None, None, 128])
+        h_v_history.set_shape([None, None, 128])
+        h_u_history.set_shape([None, None, 128])
+
+        y_e = self.gru_e(h_e_history)[1]
+        y_v = self.gru_v(h_v_history)[1]
+        y_u = self.gru_u(h_u_history)[1]
+
+        y_e_bar = tf.reduce_sum(
+                        tf.multiply(
+                            tf.tile(
+                                tf.expand_dims(
+                                    tf.where( # (n_bonds, n_mols)
+                                        tf.boolean_mask(
+                                            bond_in_mol,
+                                            tf.reduce_any(
+                                                bond_in_mol,
+                                                axis=1),
+                                            axis=0),
+                                        tf.ones_like(
+                                            tf.boolean_mask(
+                                                bond_in_mol,
+                                                tf.reduce_any(
+                                                    bond_in_mol,
+                                                    axis=1),
+                                                axis=0),
+                                            dtype=tf.float32),
+                                        tf.zeros_like(
+                                            tf.boolean_mask(
+                                                bond_in_mol,
+                                                tf.reduce_any(
+                                                    bond_in_mol,
+                                                    axis=1),
+                                                axis=0),
+                                            dtype=tf.float32)),
+                                    2),
+                                [1, 1, tf.shape(y_e)[1]]),
+                            tf.tile( # (n_bonds, n_mols, d_e)
+                                tf.expand_dims(
+                                    y_e, # (n_bonds, d_e)
+                                    1),
+                                [1, tf.shape(bond_in_mol)[1], 1])),
+                        axis=0)
+
+        y_v_bar = tf.reduce_sum(
+                tf.multiply(
+                    tf.tile(
+                        tf.expand_dims(
+                            tf.where( # (n_bonds, n_mols)
+                                atom_in_mol,
+                                tf.ones_like(
+                                    atom_in_mol,
+                                    dtype=tf.float32),
+                                tf.zeros_like(
+                                    atom_in_mol,
+                                    dtype=tf.float32)),
+                            2),
+                        [1, 1, tf.shape(y_v)[1]]),
+                    tf.tile( # (n_bonds, n_mols, d_e)
+                        tf.expand_dims(
+                            y_v, # (n_bonds, d_e)
+                            1),
+                        [1, tf.shape(atom_in_mol)[1], 1])),
+                axis=0)
+
+        y = self.d(y_e_bar, y_v_bar, y_u)
+
+        y = tf.reshape(y, [-1])
+
         return y
 
 class f_v(tf.keras.Model):
@@ -30,68 +125,71 @@ class f_v(tf.keras.Model):
         super(f_v, self).__init__()
         self.d = tf.keras.layers.Dense(units)
 
-    @tf.function
+    # @tf.function
     def call(self, x):
-        return self.d(tf.one_hot(x, 8))
+        x = tf.one_hot(x, 8)
+        x.set_shape([None, 8])
+        return self.d(x)
+
+class phi_u(tf.keras.Model):
+    def __init__(self):
+        super(phi_u, self).__init__()
+        self.d = lime.nets.for_gn.ConcatenateThenFullyConnect((128, 'elu', 128, 'elu'))
+
+    def call(self, h_u, h_u_0, h_e_bar, h_v_bar):
+        return self.d(h_u, h_u_0, h_e_bar, h_v_bar)
 
 gn = gin.probabilistic.gn.GraphNet(
     f_e=tf.keras.layers.Dense(128),
 
     f_v=f_v(128),
 
-    f_u=(lambda x, y, _: tf.zeros((1, 128), dtype=tf.float32)),
+    f_u=(lambda atoms, adjacency_map, batched_attr_mask: \
+        tf.tile(
+            tf.zeros((1, 128)),
+            [
+                 tf.math.count_nonzero(batched_attr_mask),
+                 1
+            ]
+        )),
 
     phi_e=lime.nets.for_gn.ConcatenateThenFullyConnect((128, 'elu', 128, 'elu')),
 
     phi_v=lime.nets.for_gn.ConcatenateThenFullyConnect((128, 'elu', 128, 'elu')),
 
-    phi_u=lime.nets.for_gn.ConcatenateThenFullyConnect((128, 'elu', 128, 'elu')),
+    phi_u=phi_u(),
 
-    f_r=f_r((128, 'tanh', 128, 1)),
+    f_r=f_r(128, (128, 'elu', 128, 1)),
 
     repeat=3)
 
 
+optimizer = tf.keras.optimizers.Adam(1e-5)
+@tf.function
 def train():
-    optimizer = tf.keras.optimizers.Adam(1e-5)
     n_epoch = 50
-    batch_size = 4
-    batch_idx = 0
-    loss = 0
-    tape = tf.GradientTape()
-    mols = []
 
-    for dummy_idx in range(n_epoch):
-        for atoms, adjacency_map, y in ds:
-            mol = [atoms, adjacency_map]
-            mols.append(mol)
-            batch_idx += 1
+    for epoch_idx in range(50):
+        for atoms, adjacency_map, atom_in_mol, bond_in_mol, y, y_mask in ds:
 
-            if batch_idx == batch_size:
-                loss = 0
-                idx = 0
+            with tf.GradientTape() as tape:
+                y_hat = gn(
+                    atoms,
+                    adjacency_map,
+                    atom_in_mol=atom_in_mol,
+                    bond_in_mol=bond_in_mol,
+                    batched_attr_in_mol=y_mask)
 
-                def loop_body(idx, loss):
-                    mol = mols[idx]
-                    y_hat = gn(mol[0], mol[1])
-                    loss += tf.clip_by_norm(
-                        tf.pow(y - y_hat, 2),
-                        1e8)
-                    return idx + 1, loss
+                y = tf.boolean_mask(
+                    y,
+                    y_mask)
 
-                with tf.GradientTape() as tape:
-                    _, loss= tf.while_loop(
-                        lambda idx, loss: tf.less(idx, batch_size),
-                        loop_body,
-                        [idx, loss],
-                        parallel_iterations=batch_size)
+                loss = tf.losses.mean_squared_error(y, y_hat)
 
-                print(loss)
-                variables = gn.variables
-                grad = tape.gradient(loss, variables)
-                optimizer.apply_gradients(
-                    zip(grad, variables))
-                batch_idx = 0
-                mols = []
+            logger.debug(loss)
+            variables = gn.variables
+            grad = tape.gradient(loss, variables)
+            optimizer.apply_gradients(
+                zip(grad, variables))
 
 train()
