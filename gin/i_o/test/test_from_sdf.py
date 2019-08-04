27c99f4cca9908d2adc0000e0ef1a285fb8d9ab5
Yuanqing Wang <wangyq@wangyq.net>
2019-08-04 10:54:38 -0400

    fixed bugs

--- gin/i_o/test/test_from_sdf.py
+++ gin/i_o/test/test_from_sdf.py
@@ -1,5 +1,5 @@
 import gin
-ds = gin.i_o.from_sdf.to_ds('data/mols.sdf', True)
+ds = gin.i_o.from_sdf.to_ds('data/molecule_1.sdf', True)
 ds = ds.map(lambda atoms, adjacency_map, coordinates, charges:\
     (atoms, adjacency_map, charges))
 ds = gin.probabilistic.gn.GraphNet.batch(ds, 256, per_atom_attr=True)
