79df0b504c6c7408383b78766e78276dcce375c0
Yuanqing Wang <wangyq@wangyq.net>
2019-08-05 16:39:53 -0400

    speeeeed up reading by caching

--- gin/i_o/test/test_from_sdf.py
+++ gin/i_o/test/test_from_sdf.py
@@ -1,8 +1,22 @@
 import gin
+import tensorflow as tf
+import time
+import os
+
 ds = gin.i_o.from_sdf.to_ds('/Users/yuanqingwang/Downloads/mols.sdf', True)
 ds = ds.map(lambda atoms, adjacency_map, coordinates, charges:\
     (atoms, adjacency_map, charges))
-ds = gin.probabilistic.gn.GraphNet.batch(ds, 256, per_atom_attr=True)
 
-for x in ds:
+
+# ds = ds.take(256)
+
+ds = gin.probabilistic.gn.GraphNet.batch(ds, 64, per_atom_attr=True).cache(
+    '/Users/yuanqingwang/Downloads/temp')
+
+time0 = time.time()
+for x in ds.skip(20):
     print(x)
+
+time1 = time.time()
+
+print(time1 - time0)
