1ece4548e606a0284f1325ec42919b42045575a2
yuanqing-wang <wangyq@wangyq.net>
2019-08-17 01:00:14 -0400

    sh script for model harvest

---
+++ lime/scripts/partial_charge/harvest_partial_charge.sh
@@ -0,0 +1,12 @@
+#BSUB -q gpuqueue
+#BSUB -J partial_charge
+#BSUB -m "ld-gpu ls-gpu lt-gpu lp-gpu lg-gpu lv-gpu lu-gpu"
+#####BSUB -m "lu-gpu"
+#BSUB -q gpuqueue -n 12 -gpu "num=4:j_exclusive=yes"
+#BSUB -R "rusage[mem=4] span[hosts=1]"
+######BSUB -R V100
+#BSUB -W 16:00
+#BSUB -o %J.stdout
+#BSUB -eo %J.stderr
+
+python harvest_charge.py
